05952a3db67086bce5dbd24b78c672fbe234881c
Benedict Short <benedictnjshort@gmail.com>
2025-02-07 10:17:17 +0000

    Introduce generic mapping

--- src/intrinsics.jl
+++ src/intrinsics.jl
@@ -5,17 +5,12 @@ using MLIR.IR
 using MLIR
 using MLIR.Dialects: arith, func, cf
 
-module Predicates
-const eq = 0
-const ne = 1
-const slt = 2
-const sle = 3
-const sgt = 4
-const sge = 5
-const ult = 6
-const ule = 7
-const ugt = 8
-const uge = 9
+include("mapping.jl")
+
+# generic check is fop is registered as a math function
+function is_math(fop)::Bool
+    idx = reinterpret(Int32, fop) + 1
+    return Core.Compiler.T_IFUNC[idx] == Core.Compiler.math_tfunc
 end
 
 
@@ -26,29 +21,11 @@ function cmpi_pred(predicate)
   end
 end
 
-# ops = [
-#     (:add_int, (arith.addi,)),
-#     (:sub_int, (arith.subi,)),
-# ]
 
-
-#TODO: see if its possible to extract the math_tfunc
-const calculation_intrinsics = Set([
-    arith.addi,
-    arith.subi,
-    arith.muli,
-])                                    
-
-# generic check is fop is registered as a math function
-function is_math(fop)::Bool
-    idx = reinterpret(Int32, fop) + 1
-    return CC.T_IFUNC[idx] == Core.Compiler.math_tfunc
-end
-
-# compare single operations
-function single_op_wrapper(fop)
+# insert single operations
+function single_op_wrapper(fop, target::Function)
     # if fop is a math operation, it needs to forward the return type
-    if is_math(fop)
+    if is_math(target)
         return (block::Block, args::Vector{Value}; result, location=Location()) ->
         push!(block, fop(args...; result, location))
     else
@@ -58,48 +35,17 @@ function single_op_wrapper(fop)
 end
 
 
-# CALCULATION_INTRINSICS
-
-# INTEGER INTRINSICS
-const integer_intrinsics_to_mlir = Dict([
-  Base.add_int => single_op_wrapper(arith.addi),
-  Base.sub_int => single_op_wrapper(arith.subi),
-  Base.sle_int => single_op_wrapper(cmpi_pred(Predicates.sle)),
-  Base.slt_int => single_op_wrapper(cmpi_pred(Predicates.slt)),
-  Base.mul_int => single_op_wrapper(arith.muli),
-  Base.not_int => function (block, args; location=Location())
-    arg = only(args)
-    mT = IR.type(arg)
-    T = IR.julia_type(mT)
-    ones = IR.result(
-      push!(block, arith.constant(; value=typemax(UInt64) % T, result=mT, location)),
-    )
-    return push!(block, arith.xori(arg, ones; location))
-  end,
-
-])
-
-# OPERATOR INTRINSICS
-const operator_intrinsics_to_mlir = Dict([
-  Base.:(===) => single_op_wrapper(cmpi_pred(Predicates.eq)),
-])
-
-
-
 ## conversion to MLIR
 
+# map Julia intrinsics to MLIR
 function intrinsic_to_mlir(target_function)
-  # map the intrinsic
+    if target_function in keys(operations)                        # map operations
+        return single_op_wrapper(operations[target_function], target_function)
+    elseif target_function in keys(predicate)                    # operator mappings
+        return single_op_wrapper(cmpi_pred(predicate[target_function]), target_function)
+    elseif target_function in keys(custom_intrinsics)             # custom intrinsics
+        return custom_intrinsics[target_function]
+    end
 
-  # integer mappings
-  if target_function in keys(integer_intrinsics_to_mlir)
-    return integer_intrinsics_to_mlir[target_function]
-
-  # operator mappings
-  elseif target_function in keys(operator_intrinsics_to_mlir)
-    return operator_intrinsics_to_mlir[target_function]
-
-  end
-
-  error("Intrinsic cannot be mapped to MLIR: $target_function")
+    error("Intrinsic cannot be mapped to MLIR: $target_function. Please update 'mapping.jl' create a Pull Request")
 end
---
+++ src/mapping.jl
@@ -0,0 +1,55 @@
+# Define the mapping from Julia IR -> MLIR Operations
+
+# using MLIR
+using LLVM: LLVM
+using Core: PhiNode, GotoNode, GotoIfNot, SSAValue, Argument, ReturnNode, PiNode
+using MLIR.IR
+using MLIR
+using MLIR.Dialects: arith, func, cf
+
+
+# Enumerate the Predicates
+module Predicates
+const eq = 0
+const ne = 1
+const slt = 2
+const sle = 3
+const sgt = 4
+const sge = 5
+const ult = 6
+const ule = 7
+const ugt = 8
+const uge = 9
+end
+
+
+# Julia Operations -> MLIR Operations
+const operations = Dict([
+  Base.add_int => arith.addi,
+  Base.sub_int => arith.subi,
+  Base.mul_int => arith.muli,
+])
+
+
+# Julia Predicates -> Predicate enumeration
+const predicate = Dict([
+    Base.sle_int => Predicates.sle,
+    Base.slt_int => Predicates.slt,
+    Base.:(===)  => Predicates.eq,
+])
+
+
+# Edge-case intrinsics -> MLIR (complicated cases)
+const custom_intrinsics = Dict([
+  Base.not_int => function (block, args; location=Location())
+    arg = only(args)
+    mT = IR.type(arg)
+    T = IR.julia_type(mT)
+    ones = IR.result(
+      push!(block, arith.constant(; value=typemax(UInt64) % T, result=mT, location)),
+    )
+    return push!(block, arith.xori(arg, ones; location))
+  end,
+   
+])
+
