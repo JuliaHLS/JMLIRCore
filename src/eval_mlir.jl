b1c80b2740186cc55d01d9ad396ff2c4410477db
Benedict Short <benedictnjshort@gmail.com>
2025-04-12 12:40:39 +0100

    Fixed Segfault on (GC removes underlying object prematurely) + new ExecutionEngine

--- src/eval_mlir.jl
+++ src/eval_mlir.jl
@@ -1,6 +1,17 @@
 include("code_mlir.jl")
 include("compiler.jl")
 
+already_registered = false
+
+# ensure that the lifetimes of the JIT are equal
+mutable struct OwnInst 
+    mod::Union{IR.Module, Nothing}
+    jit::Union{MLIR.API.MlirExecutionEngine, Nothing}
+    function OwnInst()
+        new(nothing, nothing)
+    end
+end
+
 "Macro @eval_mlir f(args...)"
 macro eval_mlir(call)
     @assert Meta.isexpr(call, :call) "only calls are supported"
@@ -13,70 +24,27 @@ macro eval_mlir(call)
     end
 end
 
-# function lowerToLinalg(ctx, mod)
-#     MLIR.API.mlirRegisterConversionTosaToSCF()
-#     pm = MLIR.API.mlirPassManagerCreate(ctx)
-#     if LLVM.version() >= v"15"
-#         op = "func.func"
-#     else
-#         op = "builtin.func"
-#     end
-#     opm = MLIR.API.mlirPassManagerGetNestedUnder(pm, op)
-#     # MLIR.API.mlirPassManagerAddOwnedPass(
-#     #     pm, MLIR.API.mlirCreateConversionTosaToLinalg()
-#     # )
-
-#     MLIR.API.mlirOpPassManagerAddOwnedPass(
-#     #     opm, MLIR.API.mlirCreateConversionArithToLLVMConversionPass()
-#         opm, MLIR.API.mlirCreateConversionTosaToSCF()
-#     )
-
-#     op = MLIR.API.mlirModuleGetOperation(mod)
-#    status = MLIR.API.mlirPassManagerRunOnOp(pm, op)
-
-#     println("NEW MOD: ", mod)
-#     println("NEW OP: ", op)
-#     # undefined symbol: mlirLogicalResultIsFailure
-#     if status.value == 0
-#         error("Unexpected failure running pass failure")
-#     end
-#     return MLIR.API.mlirPassManagerDestroy(pm)
-# end
-# function transform_mlir_string_with_mlir_opt(mlir_string::String, pass_pipeline::String, output_file::String)
-#     # Write the MLIR string to a temporary file
-#     open("input.mlir", "w") do io
-#         write(io, mlir_string)
-#     end
-
-#     # Execute mlir-opt
-#     command = `mlir-opt input.mlir $pass_pipeline -o $output_file`
-#     run(command)
-
-#     # Parse output MLIR
-#     output_mlir_string = read(output_file, String)
-#     output_module = MLIR.parse(MLIR.Context(), output_mlir_string)
-
-#     return output_module
-# end
 # temp solution is just to run it externally, as it is not actually part of the main pipeline
 function registerAllUpstreamDialects!(ctx)
+    println("Registering Dialects")
     if LLVM.version() >= v"15"
         registry = MLIR.API.mlirDialectRegistryCreate()
         MLIR.API.mlirRegisterAllDialects(registry)
         MLIR.API.mlirContextAppendDialectRegistry(ctx, registry)
         MLIR.API.mlirDialectRegistryDestroy(registry)
     else
-        MLIR.API.mlirRegisterAllDialects(ctx)
+        # registry = MLIR.API.mlirDialectRegistryCreate()
+        MLIR.API.mlirRegisterAllDialects()
     end
 
     return nothing
 end
 
 # externally lower TOSA to linalg (unsupported by MLIR.jl)
-function external_lowering_mlir_opt!(ops, passes::Cmd , ctx)
+function external_lowering_mlir_opt!(op::IR.Operation, passes::Cmd , ctx)
     # extract str
     mlir_buffer = IOBuffer()
-    print(mlir_buffer, ops)
+    print(mlir_buffer, op)
     mlir_str = String(take!(mlir_buffer))
 
     # write to temp file for mlir-opt to read
@@ -88,8 +56,6 @@ function external_lowering_mlir_opt!(ops, passes::Cmd , ctx)
 
 
     # lower
-    # cmd = `mlir-opt temp.mlir -o temp_out.mlir`
-    # --one-shot-bufferize --convert-linalg-to-loops --convert-to-llvm -o temp_out.mlir`
     println("Running CMD: ", passes)
     run(passes)
 
@@ -104,14 +70,26 @@ function external_lowering_mlir_opt!(ops, passes::Cmd , ctx)
     ctx = MLIR.API.mlirContextCreate()
     println("processed: 1")
     registerAllUpstreamDialects!(ctx)
+
+    println("num registered dialects ", IR.num_registered_dialects())
     println("processed: 2")
-    mod = MLIR.API.mlirModuleCreateParse(ctx , ir)
+    println("Now processing:")
+    println(ir)
+
+    println("Type of ctx: ", typeof(ctx))
+
     println("processed: 3")
 
+    mod = MLIR.API.mlirModuleCreateParse(ctx , ir)
 
-    return IR.Module(mod)
+    old_op = op.operation
+    op.operation = MLIR.API.mlirModuleGetOperation(mod)
+    MLIR.API.mlirOperationDestroy(old_op)
+
+    return 
 end
 
+fo = OwnInst()
 
 "Execute function using MLIR pipeline"
 function eval_mlir(f, args...)
@@ -138,30 +116,30 @@ function eval_mlir(f, args...)
         op::IR.Operation = code_mlir(f, arg_types) 
 
         # lower to linalg
-        mod = external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --pass-pipeline="builtin.module(func.func(tosa-to-linalg))" -o temp_out.mlir`, ctx)
-        op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
+        external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --pass-pipeline="builtin.module(func.func(tosa-to-linalg))" -o temp_out.mlir`, ctx)
 
-        mod = external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --pass-pipeline="builtin.module(one-shot-bufferize{bufferize-function-boundaries})" -o temp_out.mlir`, ctx) #--one-shot-bufferize --convert-linalg-to-loops --convert-to-llvm -o temp_out.mlir`, ctx)
-        # mod = external_lowering_mlir_opt!(op, `--one-shot-bufferize`, ctx)
-        op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
-
-        mod = external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --convert-linalg-to-affine-loops -o temp_out.mlir`, ctx)
-        op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
-
-
-        # mod = external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --convert-convert-memref-to-llvm -o temp_out.mlir`, ctx)
+        external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --pass-pipeline="builtin.module(one-shot-bufferize{bufferize-function-boundaries})" -o temp_out.mlir`, ctx) #--one-shot-bufferize --convert-linalg-to-loops --convert-to-llvm -o temp_out.mlir`, ctx)
+        # # mod = external_lowering_mlir_opt!(op, `--one-shot-bufferize`, ctx)
         # op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
 
-        mod = external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --lower-affine -o temp_out.mlir`, ctx)
-        op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
+        external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --convert-linalg-to-affine-loops -o temp_out.mlir`, ctx)
+        # op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
 
 
-        mod = external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --convert-scf-to-cf -o temp_out.mlir`, ctx)
-        op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
+        # # mod = external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --convert-convert-memref-to-llvm -o temp_out.mlir`, ctx)
+        # # op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
+
+        external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --lower-affine -o temp_out.mlir`, ctx)
+        # op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
 
 
-        mod = external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --convert-to-llvm -o temp_out.mlir`, ctx)
-        op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
+        external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --convert-scf-to-cf -o temp_out.mlir`, ctx)
+        # op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
+
+
+        external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --convert-to-llvm -o temp_out.mlir`, ctx)
+        # op = IR.Operation(MLIR.API.mlirModuleGetOperation(fo.mod))
+        
 
         # mod = external_lowering_mlir_opt!(op, `mlir-opt temp.mlir --convert-to-llvm -o temp_out.mlir`, ctx)
         # op = IR.Operation(MLIR.API.mlirModuleGetOperation(mod))
@@ -212,19 +190,18 @@ function eval_mlir(f, args...)
 
         # lowerToLinalg(ctx, mod)
 
-        # println("GOT mod: ", mod)
-
+        mod = IR.Module(op)
         # create the jit (locally)
-        jit = MLIR.API.mlirExecutionEngineCreate(mod, 0, 0, C_NULL, false)
-
-        println("created jit")
+        jit = IR.ExecutionEngine(mod, 0)
 
         # register function call within the JIT
-        MLIR.API.mlirExecutionEngineLookup(jit, nameof(f))
-        println("created handle")
+        println(String(nameof((f))))
+        println("Registration Status? ", IR.lookup(jit, String(nameof(f))))
     end
 
     println("compiled code successfully")
+
+    GC.gc()
 
     # expanded_args = eval(Expr(:tuple, args[2:end]...))
     # expanded_types = Expr(:tuple, processed_arg_types_tuple...)
