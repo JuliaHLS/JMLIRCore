e6f7b07321a5d31a7b9fbdee6d71c8b82238086c
Benedict Short <benedictnjshort@gmail.com>
2025-05-06 23:35:10 +0100

    Added prototype pass to convert tensor to SSA

--- src/julia/passes.jl
+++ src/julia/passes.jl
@@ -33,7 +33,7 @@ module JuliaPasses
 using MLIR.IR
 import MLIR.IR
 using MLIR.API
-using MLIR.Dialects: arith, tosa, tensor, math
+using MLIR.Dialects: arith, tosa, tensor, math, cf
 
 include("helpers.jl")
 
@@ -83,11 +83,10 @@ function IR.pass_run(::LowerJuliaMat, func_op)
     rewrite_references(replace_ops)  
 end
 
+
 struct LowerJuliaArith <: IR.AbstractPass end
 
 IR.opname(::LowerJuliaArith) = "func.func"
-
-
 
 function IR.pass_run(::LowerJuliaArith, func_op)
     println("Running LowerJuliaArith")
@@ -116,5 +115,313 @@ function IR.pass_run(::LowerJuliaArith, func_op)
 
     rewrite_references(replace_ops)
 end
+
+
+struct TensorToSSA <: IR.AbstractPass end
+
+IR.opname(::TensorToSSA) = "func.func"
+
+function check_name(op, target_name)::Bool
+    op_name = name(op)
+    return length(op_name) >= length(target_name) && op_name[1:length(target_name)] == target_name
+end 
+
+function get_child_blocks!(visited::Set, curr::Block, targets::Vector{IR.Block})
+    if curr.block ∈ visited
+        return
+    end
+
+    push!(visited, curr.block)
+    push!(targets, curr)
+
+    println("visited: $visited")
+    for op in IR.OperationIterator(curr)
+        # if cf.br type, traverse
+        if check_name(op, "cf.br") || check_name(op, "cf.cond_br")
+            println("processing op $op")
+            
+            for n_succ in 1:IR.nsuccessors(op)
+                target_block = IR.successor(op, n_succ)
+                println("found target_block $target_block")
+
+                if target_block ∉ visited
+                    get_child_blocks!(visited, target_block, targets)
+                end
+            end
+        end
+    end
+end
+
+get_ret(op) = IR.type.(JuliaLowerOp._JuliaPassHelpers.collect_results(op))[1]
+
+function fix_block!(original_op::Operation, block::Block)
+    # fix function signature
+    println("NOW FIXING FOR: $original_op")
+
+    IR.push_argument!(block, get_ret(original_op)) 
+
+    new_ssa::IR.Value = IR.argument(block, IR.nargs(block))
+    println("TYPE TYPE: $new_ssa with type")
+    new_op = original_op
+    println("############################################")
+    println("starting with op: $new_op")
+
+    println("Processing: $block")
+
+    for op in IR.OperationIterator(block)
+        operands = JuliaLowerOp._JuliaPassHelpers.collect_operands(op)
+
+        for (i, present_operand) in enumerate(operands)
+            if (present_operand) === IR.result(original_op) && !(check_name(op, "func.return"))
+                IR.operand!(op, i, new_ssa)
+
+                # println("s: $(size(get_ret(op)))")
+                # println("$new_op")
+                # println("s2: $(size(get_ret(new_op)))")
+                println("op: $(IR.julia_type(get_ret(op)) <: AbstractArray)")
+                if IR.julia_type(get_ret(op)) <: AbstractArray && size(get_ret(op)) == size(get_ret(new_op))
+                    old_ssa = new_ssa
+                    println("UPDATED")
+                    new_ssa = IR.result(op)
+                    new_op = op
+                    println("new dims: from $(size(old_ssa)) to $(size(new_ssa))")
+                    if size(old_ssa) != size(new_ssa)
+                        error("HERE")
+                    end
+
+                    println("#########################: $new_ssa, $old_ssa")
+                end
+            elseif new_ssa == present_operand && !(check_name(op, "cf.br") || check_name(op, "cf.cond_br") ) # if SSA chain is found, progress
+                println("op: $op")
+
+                println("s: $(size(get_ret(op)))")
+                println("$new_op")
+                println("s2: $(size(get_ret(new_op)))")
+                if IR.julia_type(get_ret(op)) <: AbstractArray && size(get_ret(op)) == size(get_ret(new_op))
+                    old_ssa = new_ssa
+                    new_ssa = IR.result(op)
+                    new_op = op
+                    println("#########################: $new_ssa, $old_ssa")
+                end
+            end
+        end
+        
+        if check_name(op, "func.return")
+            operands = JuliaLowerOp._JuliaPassHelpers.collect_operands(op)
+            @assert length(operands) == 1
+
+            println("operands: $operands with size $(size(operands[1]))")
+
+            if size(operands[1]) == size(new_ssa)
+                new_operands = [new_ssa]
+                API.mlirOperationSetOperands(op, length(new_operands), new_operands)
+            end
+
+            
+        end
+
+        if check_name(op, "cf.br") || check_name(op, "cf.cond_br")
+            # states= API.h
+            # API.mlirOperationStateAddOperands(state, length(operands), operands)
+            println("PROCESSING WITH OPERANDS: $operands for $op with new ssa : $new_ssa")
+            
+            # fix the operands here to be in the right order
+
+
+            if check_name(op, "cf.cond_br")
+
+                println("Got ssa: $new_ssa")
+
+                halfway_length = Int32((length(operands) - 1) / 2)
+                new_operands = [operands[1:(halfway_length+1)]...]
+                push!(new_operands,new_ssa)
+                push!(new_operands, operands[(halfway_length + 2):end]...)
+                push!(new_operands, new_ssa)
+
+                println("working: $new_operands")
+
+                # new_operands = push!(operands, new_ssa)
+                API.mlirOperationSetOperands(op, length(new_operands), new_operands)
+
+                # println("checking op: $op")
+                # println("num attrs: $(IR.nattrs(op))")
+                # println("Got attributes: $(IR.attr(op, "operandSegmentSizes"))")
+
+                # fix attributes
+                at = IR.attr(op, "operandSegmentSizes")
+
+                cond = Int32(at[0])
+                first_args = Int32(at[1])
+                second_args = Int32(at[2])
+
+                first_args += 1
+                second_args += 1
+
+                new_at = IR.DenseArrayAttribute(Int32.([cond, first_args, second_args])::Vector{Int32})
+                # IR.rmattr!(op, "operandSegmentSizes")
+                IR.attr!(op, "operandSegmentSizes", new_at)
+            else
+                new_operands = push!(operands, new_ssa)
+                API.mlirOperationSetOperands(op, length(new_operands), new_operands)
+            end
+
+
+            # if check_name(op, "cf.br")
+            #     # cond_br = cf.br(
+            #     #         values, 
+            #     #         dest=destination
+            #     #     )
+            # end
+                
+        end
+    end
+
+    println("now block is: $block")
+
+    println("got terminator op: $new_ssa")
+
+
+
+    # todo: find the last SUCCESSOR of the IR operation in the block
+
+    # modify return op to add the new op
+end
+
+using MLIR
+
+function cond_br(
+    operands::Vector{Value};
+    trueDest::Block,
+    falseDest::Block,
+    location=Location(),
+)
+    _results = IR.Type[]
+    _operands = Value[operands...]
+    _owned_regions = Region[]
+    _successors = Block[trueDest, falseDest]
+    _attributes = IR.NamedAttribute[]
+    push!(
+        _attributes,
+        MLIR.Dialects.operandsegmentsizes([1, length(trueDestOperands), length(falseDestOperands)]),
+    )
+
+    return IR.create_operation(
+        "cf.cond_br",
+        location;
+        operands=_operands,
+        owned_regions=_owned_regions,
+        successors=_successors,
+        attributes=_attributes,
+        results=_results,
+        result_inference=false,
+    )
+end
+
+
+
+function postfix_block!(original_op::Operation, block::Block)
+    # IR.push_argument!(block, get_ret(original_op)) 
+    new_ssa = IR.argument(block, IR.nargs(block))
+    replace_ops = []
+
+    for op in IR.OperationIterator(block)
+        operands = JuliaLowerOp._JuliaPassHelpers.collect_operands(op)
+        results = JuliaLowerOp._JuliaPassHelpers.collect_results(op)
+        if check_name(op, "cf.br") || check_name(op, "cf.cond_br")
+            # states= API.h
+            #
+
+            succ = []
+            for n_succ in 1:IR.nsuccessors(op)
+                push!(succ, IR.successor(op, n_succ))
+            end
+
+            println("Got operands: $operands and results $succ")
+            # API.mlirOperationStateAddOperands(state, length(operands), operands)
+            println("PROCESSING WITH OPERANDS: $operands for $op")
+
+            if check_name(op, "cf.br")
+                new_op = cf.br(operands, dest=succ[1])
+            elseif check_name(op, "cf.cond_br")
+                len = length(succ)
+                new_op = cond_br(operands; trueDest=succ[(len-1)], falseDest=last(succ))
+                # new_op = cf.br(operands, dest=succ[1])
+            else
+                error("Unrecognised cf.br detected $op")
+            end
+
+            IR.insert_before!(block, op, new_op)
+
+            push!(replace_ops, [op, new_op])
+
+
+            # rewrite_references(replace_ops)
+
+            # new_operands = push!(operands, new_ssa)
+            # API.mlirOperationSetOperands(op, length(operands), operands)
+
+            # if check_name(op, "cf.br")
+            #     # cond_br = cf.br(
+            #     #         values, 
+            #     #         dest=destination
+            #     #     )
+            # end
+                
+        end
+    end
+end
+
+function IR.pass_run(::TensorToSSA, func_op)
+    println("Running TensorToSSA")
+
+    # store dominating blocks
+    dominating_blocks::Vector{Any} = []
+
+    for region in IR.RegionIterator(func_op)
+        for block in IR.BlockIterator(region)
+            for op in IR.OperationIterator(block)
+                op_name = name(op)
+
+                if length(op_name) >= 14 && op_name[1:14] == "julia.mat_inst"
+                    push!(dominating_blocks, [block, op])
+                end
+            end
+        end
+    end
+
+    # collect branches
+    target_collection = []
+    for (dominator, dominating_op) in dominating_blocks 
+        visited = Set()
+        targets = Vector{IR.Block}()
+        get_child_blocks!(visited, dominator, targets)
+
+        push!(target_collection, targets)
+    end
+
+    # correct branch
+    for (dom_block, collection) in zip(dominating_blocks, target_collection)
+        # println("First: $((collection)[2:end])")
+        println("HERHEHEHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH: $(last(dom_block))")
+        for op in IR.OperationIterator(collection[1])
+            operands = JuliaLowerOp._JuliaPassHelpers.collect_operands(op)
+
+            if check_name(op, "cf.br") || check_name(op, "cf.cond_br")
+                println("PROCESSING WITH OPERANDS: $operands for $(last(dom_block))")
+                new_operands = push!(operands, IR.result(last(dom_block)))
+                API.mlirOperationSetOperands(op, length(new_operands), new_operands)
+            end
+        end
+
+        for target_block in collection[2:end]
+            fix_block!(last(dom_block), target_block)
+            # postfix_block!(last(dom_block), target_block)
+        end
+    end
+
+    # rewrite tree
+
+end
+
 
 end
